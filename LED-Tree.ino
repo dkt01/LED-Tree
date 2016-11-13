6b529669d742f3e49a85101eebe63711b59d3155
dkt01 <d.k.turner@att.net>
2016-11-13 16:22:29 -0600

    Added basic ethernet control.

--- LED-Tree.ino
+++ LED-Tree.ino
@@ -1,3 +1,6 @@
+#include <EtherCard.h>
+#include <IPAddress.h>
+
 #include <Adafruit_NeoPixel.h>
 #include "NeoPixelRing.h"
 #ifdef __AVR__
@@ -5,6 +8,16 @@
 #endif
 
 #define PIN 9
+
+// IP Address configuration
+static byte myIP[] = {165,26,78,253};
+static byte gwIP[] = {165,26,78,254}; // Not used
+
+// MAC Address
+static byte myMAC[] = {0x6,0x5,0x4,0x3,0x2,0x1};
+
+// Send/receive buffer
+byte Ethernet::buffer[500];
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -22,9 +35,100 @@ NeoPixelRing tree = NeoPixelRing(PIN, NEO_GRB + NEO_KHZ800, 6, ringsArray);
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 
+//callback that prints received packets to the serial port
+void udpDataReceived(uint16_t dest_port, uint8_t src_ip[IP_LEN], uint16_t src_port, const char *data, uint16_t len){
+  IPAddress src(src_ip[0],src_ip[1],src_ip[2],src_ip[3]);
+
+  // Serial.print("dest_port: ");
+  // Serial.println(dest_port);
+  // Serial.print("src_port: ");
+  // Serial.println(src_port);
+
+  // Serial.print("src_ip: ");
+  // ether.printIp(src_ip);
+
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+  NeoPixelRing::Pattern pattern;
+  uint8_t param;
+
+  // 0x0 : red value
+  // 0x1 : green value
+  // 0x2 : blue value
+  // 0x3 : pattern
+  // 0x4 : param
+  if(len != 6)
+  {
+    // Serial.write("\nData not equal to 6 bytes. Received:");
+    // Serial.println(data);
+    // Serial.write("\nLength:");
+    // Serial.print(len);
+  }
+  for(uint8_t i = 0; i < len; i++)
+  {
+    switch (i)
+    {
+        case 0:
+          red = data[i];
+          // Serial.write("\nRed:");
+          // Serial.print(red);
+          break;
+        case 1:
+          green = data[i];
+          // Serial.write("\nGreen:");
+          // Serial.print(green);
+          break;
+        case 2:
+          blue = data[i];
+          // Serial.write("\nBlue:");
+          // Serial.print(blue);
+          break;
+        case 3:
+          pattern = data[i];
+          // Serial.write("\nPattern:");
+          // Serial.print(pattern);
+          break;
+        case 4:
+          param = data[i];
+          // Serial.write("\nParam:");
+          // Serial.print(param);
+          break;
+        default:
+          break;
+    }
+  }
+
+  uint32_t color = ((uint32_t)red)   << 16 |
+                   ((uint32_t)green) << 8  |
+                   ((uint32_t)blue)  << 0;
+
+  for(uint8_t i = 0; i < (sizeof(ringsArray) - 1); i++)
+  {
+    tree.setPattern(i,
+                    pattern,
+                    color,
+                    2000,
+                    param);
+  }
+}
+
 void setup()
 {
   Serial.begin(9600);
+
+  if(ether.begin(sizeof Ethernet::buffer, myMAC, 10) == 0)
+  {
+    Serial.println(F("Failed to access Ethernet controller"));
+  }
+  ether.staticSetup(myIP);
+
+  ether.printIp("IP: ", ether.myip);
+  ether.printIp("GW: ", ether.gwip);
+  ether.printIp("DNS: ", ether.dnsip);
+
+  ether.udpServerListenOnPort(&udpDataReceived, 8733);
+
   tree.begin();
   tree.setBrightness(75);
   tree.update(); // Initialize all pixels to 'off'
@@ -64,17 +168,12 @@ void loop()
 {
   // static uint8_t progress = 0;
   // static uint8_t loopCount = 0;
-  // if(loopCount % 10 == 0)
+  ether.packetLoop(ether.packetReceive());
+  tree.update();
+  // if(loopCount % 30 == 0)
   // {
-  //   progress++;
-  //   progress = progress % 100;
-  //   tree.setPattern(2,
-  //                   NeoPixelRing::PROGRESS,
-  //                   0x0000FF,
-  //                   2000,
-  //                   progress);
+  //   Serial.write("\nHI");
+  //   Serial.print(loopCount);
   // }
   // loopCount++;
-  tree.update();
-  delay(10);
 }
